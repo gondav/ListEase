615267be6c889905be1ed0e3b9b0832736e667af
gondav <viktoria.gonda@gmail.com>
2024-04-12 17:53:43 +0200

    font awesome setup

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,17 @@ import './assets/main.scss';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
+
+/* add icons to the library */
+library.add(faSquare, faSquareCheck);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
