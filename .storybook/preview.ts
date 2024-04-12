49a75d02d9c241b1de27df3769f556aeaa046e95
gondav <viktoria.gonda@gmail.com>
2024-04-12 19:16:56 +0200

    font awesome setup in storybook preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,24 @@
-import type { Preview } from "@storybook/vue3";
+import { setup, Preview, StoryFn } from '@storybook/vue3';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+setup((app) => {
+  //👇 Adds the icon to the library so you can use it in your story.
+  library.add(far);
+  app.component('font-awesome-icon', FontAwesomeIcon);
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: { expanded: true },
+  decorators: [
+    (story: StoryFn) => ({
+      components: { story },
+      template: '<div style="margin: 3em;"><story /></div>',
+    }),
+  ],
+};
 
 const preview: Preview = {
   parameters: {
