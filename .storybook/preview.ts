93e5ad1b2f5bef11504492f976e334045c72dd18
gondav <viktoria.gonda@gmail.com>
2024-04-14 18:01:22 +0200

    add xmark and caret-right icons

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,18 +1,19 @@
-import { setup, Preview } from '@storybook/vue3';
+import { setup, Preview, StoryObj } from '@storybook/vue3';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 setup((app) => {
   //👇 Adds the icon to the library so you can use it in your story.
-  library.add(far);
+  library.add(far, fas);
   app.component('font-awesome-icon', FontAwesomeIcon);
 });
 
 export const parameters = {
   controls: { expanded: true },
   decorators: [
-    (story) => ({
+    (story: StoryObj) => ({
       components: { story },
       template: '<div style="margin: 3em;"><story /></div>',
     }),
--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,10 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 /* import specific icons */
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
-library.add(faSquare, faSquareCheck);
+library.add(faSquare, faSquareCheck, faXmark, faCaretRight);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
